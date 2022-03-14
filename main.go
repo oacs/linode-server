8049e245ee817019753cb1c99b9222d23ef14fc8
Oscar C <oacs1221@gmail.com>
2022-03-14 19:40:44 +0100

    feat: add production args

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"example.com/m/v2/modules/server"
-	log "github.com/sirupsen/logrus"
 	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	args := os.Args
+	envDir := ".env"
+	if len(args) > 1 {
+		if args[1] == "--prod" {
+			envDir = "/home/www/.env"
+		}
+	}
+
+	err := godotenv.Load(envDir)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	server.Main()
 }
